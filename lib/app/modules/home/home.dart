27d6fdbe4310d6011e78b18367187d4a0cdafda5
Muller <rm80491@fiap.com.br>
2021-06-29 17:44:45 -0300

    ensure that main will start to power on correctly.

--- lib/app/modules/home/home.dart
+++ lib/app/modules/home/home.dart
@@ -1,2 +1,3 @@
 export './home_page.dart';
 export './home_binding.dart';
+export './home_controller.dart';
--- lib/app/modules/home/home_binding.dart
+++ lib/app/modules/home/home_binding.dart
@@ -1,6 +1,10 @@
 import 'package:get/get.dart';
 
+import 'home.dart';
+
 class HomeBinding implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.lazyPut(() => HomeController());
+  }
 }
